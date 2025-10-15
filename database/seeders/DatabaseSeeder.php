7193ae677a95aa8ce0cae3676dfecd1e4ddbf074
Afdanu <arsab201@gmail.com>
2025-10-15 10:45:55 +0700

    Merge remote-tracking branch 'origin/main'

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,13 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        $this->call(UserSeeder::class);
+        $this->call([
+            UserSeeder::class,
+            // ProductSeeder::class,
+        ]);
+        // \App\Models\Berita::factory(20)->create();
     }
 }
